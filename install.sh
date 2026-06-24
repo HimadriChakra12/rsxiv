55cf35c5fa595e203477be4bab915054c5953925
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-06-24 08:59:15 +0600

    safer link ensure

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 make clean
 sudo make install
 sudo cp ./etc/rsxiv.desktop /usr/share/applications 
+rm $HOME/.config/rsxiv
 mkdir -p $HOME/.config/rsxiv/
 ln -s $(pwd)/exec $HOME/.config/rsxiv/exec
