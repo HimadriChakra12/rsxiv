9f02b509f761d7b0e7091f1a7514681390280e96
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-06-24 08:58:28 +0600

    fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 make clean
 sudo make install
-sudo cp .etc/rsxiv.desktop /usr/share/applications 
+sudo cp ./etc/rsxiv.desktop /usr/share/applications 
 mkdir -p $HOME/.config/rsxiv/
 ln -s $(pwd)/exec $HOME/.config/rsxiv/exec
